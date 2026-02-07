d23f3a415b38427fc543c217794b97cccaeb421b
Nestum447 <ncarpio935@gmail.com>
2026-02-06 22:28:45 -0600

    Create InventoryViewModel.kt

---
+++ app/src/main/java/com/nestor/ocrinventarioa12/InventoryViewModel.kt
@@ -0,0 +1,26 @@
+package com.nestor.ocrinventarioa12
+
+import android.app.Application
+import androidx.lifecycle.*
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+class InventoryViewModel(application: Application) :
+    AndroidViewModel(application) {
+
+    private val dao = AppDatabase.getDatabase(application).inventoryDao()
+
+    val allItems: LiveData<List<InventoryEntity>> = dao.getAll()
+
+    fun insert(text: String) {
+        viewModelScope.launch(Dispatchers.IO) {
+            dao.insert(InventoryEntity(text = text))
+        }
+    }
+
+    fun deleteAll() {
+        viewModelScope.launch(Dispatchers.IO) {
+            dao.deleteAll()
+        }
+    }
+}
