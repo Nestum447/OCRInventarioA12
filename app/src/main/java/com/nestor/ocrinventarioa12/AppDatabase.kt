5f954a3d8af50865250da69d23d12f15d2c6f706
Nestum447 <ncarpio935@gmail.com>
2026-02-06 22:27:46 -0600

    Create AppDatabase.kt

---
+++ app/src/main/java/com/nestor/ocrinventarioa12/AppDatabase.kt
@@ -0,0 +1,30 @@
+package com.nestor.ocrinventarioa12
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database(entities = [InventoryEntity::class], version = 1)
+abstract class AppDatabase : RoomDatabase() {
+
+    abstract fun inventoryDao(): InventoryDao
+
+    companion object {
+
+        @Volatile
+        private var INSTANCE: AppDatabase? = null
+
+        fun getDatabase(context: Context): AppDatabase {
+            return INSTANCE ?: synchronized(this) {
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    AppDatabase::class.java,
+                    "inventory_database"
+                ).build()
+                INSTANCE = instance
+                instance
+            }
+        }
+    }
+}
