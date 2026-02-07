8d4f8b1e0455666e250a5a7f6888896a3b6067f5
Nestum447 <ncarpio935@gmail.com>
2026-02-06 22:26:56 -0600

    Create InventoryDao.kt

---
+++ app/src/main/java/com/nestor/ocrinventarioa12/InventoryDao.kt
@@ -0,0 +1,17 @@
+package com.nestor.ocrinventarioa12
+
+import androidx.lifecycle.LiveData
+import androidx.room.*
+
+@Dao
+interface InventoryDao {
+
+    @Insert
+    suspend fun insert(item: InventoryEntity)
+
+    @Query("SELECT * FROM inventory ORDER BY id DESC")
+    fun getAll(): LiveData<List<InventoryEntity>>
+
+    @Query("DELETE FROM inventory")
+    suspend fun deleteAll()
+}
